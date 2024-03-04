e4163120702fb217818395812837ed2d8163235c
TechSage369 <rainishchal9@gmail.com>
2024-03-04 14:47:50 +0530

    Refactor: Restructure validate_credentials into a class - Organize validate_credentials functionality into a class - Add new files to .gitignore

--- modules/validate_credentials.py
+++ modules/validate_credentials.py
@@ -1,131 +1,103 @@
-import os.path
-
-from log.logger_config import logger
+import os
+import sys
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.errors import HttpError
-from googleapiclient.discovery import build
+from googleapiclient.discovery import build, Resource
 import settings
+from log.logger_config import logger
+from typing import Any
 
-'''
 
-service = get_service() 
-
-By calling get_services() will load credentials and build the service
-'''
-# If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
-
-
-# File paths
 TOKEN_FILE_PATH = settings.BASE_DIR / 'modules' / 'secrets' / 'token.json'
-
-
 CREDENTIALS_FILE_PATH = settings.BASE_DIR / \
     'modules' / 'secrets' / 'credentials.json'
 
 
-def get_service():
+class GoogleCalendarService:
     '''
-    Get a Google Calendar service object.
+    All the Google Credentials are packed inside this class
 
-    Returns:
-        Credentials: The Google Calendar service object.
+    ## Example
+    ```
+        google_cal_service = GoogleCalendarService()
+        service = google_cal_service.get_service()
 
-    Raises:
-        Exception: If there is an error loading the credentials.
+        Then
+
+        service.events().insert(calendarId=calendarId, body=event_data).execute()
+    ```
 
     '''
-    try:
-        creds = load_credentials()
-        service = build("calendar", "v3", credentials=creds)
-        return service
 
-    except Exception as e:
-        logger.error(f"Error loading credentials from token.json: {e}")
-        raise Exception(e)
+    def __init__(self) -> None:
+        self.SCOPES = SCOPES
+        self.TOKEN_FILE_PATH = TOKEN_FILE_PATH
+        self.CREDENTIALS_FILE_PATH = CREDENTIALS_FILE_PATH
+        self.logger = logger
 
+    def get_service(self) -> Any:
+        try:
+            creds = self.load_credentials()
+            service = build("calendar", "v3", credentials=creds)
+            return service
+        except Exception as e:
+            self.handle_error('Exception', f'An exception occurred: {e}')
 
-def load_credentials():
-    '''
-    Load Google API credentials from a token file.
+    def load_credentials(self):
+        creds = None
+        if not os.path.exists(self.TOKEN_FILE_PATH):
+            self.handle_error(
+                'FileNotFound', f'file ({self.TOKEN_FILE_PATH}) not found')
 
-    Returns:
-        Credentials: The loaded credentials object.
-
-    Raises:
-        Exception: If there is an error loading the credentials.
-    '''
-    creds = None
-
-    if os.path.exists(TOKEN_FILE_PATH):
         try:
             creds = Credentials.from_authorized_user_file(
-                TOKEN_FILE_PATH, SCOPES)
+                self.TOKEN_FILE_PATH, self.SCOPES)
         except Exception as e:
-            logger.error(f"Error loading credentials from token.json: {e}")
-            raise Exception(e)
+            self.handle_error('CredentialsError',
+                              f'Error loading credentials: {e}')
 
-    if not creds or not creds.valid:
-        creds = refresh_or_acquire_new_credentials(creds)
+        if not creds or not creds.valid:
+            creds = self.refresh_or_acquire_new_credentials(creds)
+        return creds
 
-    return creds
+    def refresh_or_acquire_new_credentials(self, creds):
+        if creds and creds.expired and creds.refresh_token:
+            try:
+                creds.refresh(Request())
+            except HttpError as e:
+                self.handle_error(
+                    'HttpError', f'HTTP error occurred during refresh: {e}')
+            except Exception as e:
+                self.handle_error(
+                    'RefreshError', f'Error refreshing credentials: {e}')
+        else:
+            try:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    self.CREDENTIALS_FILE_PATH, self.SCOPES)
+                creds = flow.run_local_server(port=0)
+                self.save_credentials(creds)
+            except Exception as e:
+                self.handle_error('FlowError', f'Error running flow: {e}')
 
+        return creds
 
-def refresh_or_acquire_new_credentials(creds):
-    '''
-    Refresh expired credentials or acquire new credentials.
-
-    Args:
-        creds (Credentials): The current credentials object.
-
-    Returns:
-        Credentials: The refreshed or new credentials object.
-
-    Raises:
-        Exception: If there is an error refreshing or acquiring new credentials
-    '''
-    if creds and creds.expired and creds.refresh_token:
+    def save_credentials(self, creds):
         try:
-            creds.refresh(Request())
-        except HttpError as e:
-            logger.error({'status_code': e.resp.status, 'message': e.resp})
-            raise Exception(e)
-    else:
-        try:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                CREDENTIALS_FILE_PATH, SCOPES)
-            creds = flow.run_local_server(port=0)
-
+            with open(self.TOKEN_FILE_PATH, "w") as token_file:
+                token_file.write(creds.to_json())
         except Exception as e:
-            logger.error(f"Error running flow: {e}")
-            raise Exception(e)
+            self.handle_error('SaveCredentialsError',
+                              f'Error saving credentials to token.json: {e}')
 
-    save_credentials(creds)  # type: ignore
-    return creds
-
-
-def save_credentials(creds) -> None:
-    '''
-    Save credentials to a token file.
-
-    Args:
-        creds (Credentials): The credentials object to save.
-
-    Raises:
-        Exception: If there is an error saving the credentials.
-
-    '''
-
-    try:
-        with open(TOKEN_FILE_PATH, "w") as token:
-            token.write(creds.to_json())
-
-    except Exception as e:
-        logger.error(f"Error saving credentials to token.json: {e}")
-        raise Exception("Error while saving credential to token.json")
+    def handle_error(self, error_type, error_message):
+        print({'status': error_type, 'message': error_message})
+        self.logger.error({'status': error_type, 'message': error_message})
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    print(get_service())
+    service = GoogleCalendarService()
+    print(service.get_service())
